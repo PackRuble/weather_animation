ca7bdc3e4c2adc62a3b47908a688eb00f922ddfb
Ruble <pack.ruble@gmail.com>
2026-01-19 12:03:13 +0300

    upd: recreate weathunits_configurator using Flutter 3.38.5

--- example/weathunits_configurator/android/app/src/main/kotlin/com/example/weathunits_configurator/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weathunits_configurator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ example/weathunits_configurator/android/app/src/main/kotlin/com/rublepack/weathunits_configurator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rublepack.weathunits_configurator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
